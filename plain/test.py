57c126fe616cfaedfc38d33f149df648db8f2c03
Tarek Saier <tareksaier@gmail.com>
2016-09-20 15:55:25 +0200

    plural w/ ngettext

--- plain/test.py
+++ plain/test.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import gettext
+import sys
 
 t = gettext.translation('test', localedir='translations', fallback='true')
 _ = t.gettext
 
 print(_("This is a test."))
+
+num = int(sys.argv[1])
+print(t.ngettext('You have {numUnreadMsgs} unread Message.', 'You have {numUnreadMsgs} unread Messages.', num).format(numUnreadMsgs=num))
